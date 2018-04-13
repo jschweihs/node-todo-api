b2e7c304db8dc6cd0aacb807c6798ff651d57562
Jake Schweihs <jschweihs@gmail.com>
2018-04-13 18:38:53 -0500

    Add POST /users/login route

--- server/models/user.js
+++ server/models/user.js
@@ -72,6 +72,27 @@ UserSchema.statics.findByToken = function (token) {
   });
 };
 
+// Return a user given email and password
+UserSchema.statics.findByCredentials = function (email, password) {
+  var User = this;
+
+  return User.findOne({email}).then((user) => {
+    if (!user) {
+      return Promise.reject();
+    }
+
+    return new Promise((resolve, reject) => {
+      bcrypt.compare(password, user.password, (err, res) => {
+        if (res) {
+          resolve(user);
+        } else {
+          reject();
+        }
+      });
+    });
+  });
+};
+
 // Hash and salt password
 UserSchema.pre('save', function (next) {
   var user = this;
